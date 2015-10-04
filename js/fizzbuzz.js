cf8e139a350b5a5cf0603c8f70b19b0820368cf9
a-minor-threat <dennis.k.shah@gmail.com>
2015-10-03 23:48:01 -0400

    initial commit. it's done.

---
+++ js/fizzbuzz.js
@@ -0,0 +1,18 @@
+window.onload = (function(){
+
+var fizzbuzzArray=[];
+for (var i=1;i<=100;i++)
+{
+    if (i%3 === 0 && i%5 === 0)
+    {
+        document.write("<br> fizzbuzz");
+    } else if (i%5 === 0) {
+        document.write("<br> buzz");
+    } else if (i%3 === 0) {
+        document.write("<br> fizz");
+    } else {
+        document.write("<br>",i);
+    }
+
+}
+});
